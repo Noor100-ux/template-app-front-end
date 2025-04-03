42e0fdfc99b81af890a8245733f38eb233eb0d8b
Noor100-ux <112697081+Noor100-ux@users.noreply.github.com>
2025-04-03 15:58:53 +0530

    Update login.jsx

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -23,7 +23,7 @@ function Login() {
         setError('Please enter both email and password');
         return;
       }
-      const response = await fetch('http://localhost:5055/login', {
+      const response = await fetch('https://template-app-backend-cts1.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
